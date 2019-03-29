21ea99017814d3f3217717ce7a21ab82ccefe503
Connor Barrett <cbarrett3@wisc.edu>
2019-03-28 20:00:09 -0500

    personalinfo setup

--- src/app/personalinfo/personalinfo.component.ts
+++ src/app/personalinfo/personalinfo.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-personalinfo',
-  templateUrl: './personalinfo.component.html',
-  styleUrls: ['./personalinfo.component.scss']
+  selector: "app-personalinfo",
+  templateUrl: "./personalinfo.component.html",
+  styleUrls: ["./personalinfo.component.scss"]
 })
 export class PersonalinfoComponent implements OnInit {
+  // TODO Input Decorator once we get data model
 
-  constructor() { }
+  userName = "Jay Example";
+  userTitle = "Example grade Example at Example School";
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
