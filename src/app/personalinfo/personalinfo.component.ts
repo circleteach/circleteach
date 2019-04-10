f6e24747b07096884b7cc66acfdc7e825c70617b
Shriyans Lenkala <shriyanslenkala@yahoo.com>
2019-04-09 22:14:19 -0500

    add friends button

--- src/app/personalinfo/personalinfo.component.ts
+++ src/app/personalinfo/personalinfo.component.ts
@@ -8,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class PersonalinfoComponent implements OnInit {
   // TODO Input Decorator once we get data model
 
+  friends = false;
   userName = 'Jay Example';
   userTitle = 'Example grade Example at Example School';
 
   constructor() {}
 
   ngOnInit() {}
+
+  toggleFriend(){
+    if (this.friends){
+      this.friends = false;
+    }
+    else{
+      this.friends = true;
+    }
+  }
 }
