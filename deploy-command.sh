b1fd770fd1c435367d5eba25a4a1af2c3110ba74
Parker Lougheed <parkermeronat@gmail.com>
2019-04-12 14:10:29 -0500

    Try to get deploy setup for non dev and pull requests

--- deploy-command.sh
+++ deploy-command.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" != "dev" || "$TRAVIS_PULL_REQUEST" != "false"]; then
   firebase deploy --token $FIREBASE_TOKEN
+  npm run lh -- https://circle-teach.firebaseapp.com
 fi
