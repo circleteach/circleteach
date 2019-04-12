5552fdc21c18dbdd0d98322b841ff8ad7914e4d8
Parker Lougheed <parkermeronat@gmail.com>
2019-04-12 14:15:31 -0500

    Can only run lighthouse on pull requests

--- deploy-command.sh
+++ deploy-command.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" != "dev" || "$TRAVIS_PULL_REQUEST" != "false"]; then
+if [ "$TRAVIS_BRANCH" != "dev"]; then
   firebase deploy --token $FIREBASE_TOKEN
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false"]; then
   npm run lh -- https://circle-teach.firebaseapp.com
 fi
