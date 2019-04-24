b8cbca79e26a796bec814f373d577cee1134b0b7
Parker Lougheed <parkermeronat@gmail.com>
2019-04-23 22:28:17 -0500

    Add error messages for signup validation issues - Fixes #2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   signupPassHide = true;
   signupEmail = new FormControl('', [Validators.required, Validators.email]);
   signupPassword = new FormControl('', [Validators.required, Validators.minLength(6)]);
-  signupName = new FormControl('', [Validators.required]);
+  signupName = new FormControl('', [Validators.required, Validators.minLength(2)]);
 
   constructor(private auth: AuthenticationService) { }
 
@@ -24,16 +24,40 @@ export class LoginComponent implements OnInit {
     this.signupFailed = false;
   }
 
-  getNameErrorMessage() {
-
+  getNameErrorMessage(): string {
+    if (this.signupName.hasError('required')) {
+      return 'You must enter an name!';
+    } else if (this.signupName.hasError('minlength')) {
+      return 'Your name must be at least two characters long!';
+    } else if (this.signupName.errors != null) {
+      return 'There was a problem with your display name!';
+    } else {
+      return '';
+    }
   }
 
-  getEmailErrorMessage() {
-
+  getEmailErrorMessage(): string {
+    if (this.signupEmail.hasError('required')) {
+      return 'You must enter an email!';
+    } else if (this.signupEmail.hasError('email')) {
+      return 'Your email must be a valid email!';
+    } else if (this.signupEmail.errors != null) {
+      return 'There was a problem with your email!';
+    } else {
+      return '';
+    }
   }
 
-  getPasswordErrorMessage() {
-
+  getPasswordErrorMessage(): string {
+    if (this.signupPassword.hasError('required')) {
+      return 'You must enter a password!';
+    } else if (this.signupPassword.hasError('minlength')) {
+      return 'Your password must be at least 6 characters long!';
+    } else if (this.signupPassword.errors != null) {
+      return 'There was a problem with your password!';
+    } else {
+      return '';
+    }
   }
 
   doSignup() {
